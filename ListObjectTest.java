576153a83e18f62cf7687db76a726c986fa05c4d
alkurdya400 <80788370+alkurdya400@users.noreply.github.com>
2021-03-17 12:01:24 +0000

    Update ListObjectTest.java

--- ListObjectTest.java
+++ ListObjectTest.java
@@ -1,5 +1,6 @@
 package Testing;
 
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -7,16 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
+//A test class to test the ObjectList class
+
 class ObjectListTest {
 	private Object[] list =new Object[3];
 	private int total ;
-	
+
+	//Constructor of the test class
+	@Test
 	void testObjectList1(int sizeIn)
 	{
 		list = new Object[sizeIn];
 		total = 0;
 	}
 	
+	//A test case to test the constructor
 	@Test
 	void testObjectList() {
 		ObjectListTest ol=new ObjectListTest();
@@ -25,7 +32,8 @@ class ObjectListTest {
 		
 	}
 
-    @Test
+	// A test method named testAdd which is a reflection of the method add which is tested by the testcase testAdd1  
+     @Test
 	boolean testAdd(int n) {
 		 n=1;
 		ObjectListTest ol=new ObjectListTest();
@@ -41,6 +49,7 @@ class ObjectListTest {
 		}
 	}
 
+	//A test case to test the above method named testAdd
     @Test
     void testAdd1()
     {
@@ -51,13 +60,15 @@ class ObjectListTest {
        assertTrue(ol.testAdd(0));
     }
     
-
+	// A test case to test the testIsEmpty method of this test class
+   @Test
     void testIsEmpty1()
     {
     	ObjectListTest ol=new ObjectListTest();
     	assertTrue(ol.testIsEmpty());
     }
 
+	// A test method named testIsEmpty which is a reflection of the isEmpty method of the ObjectList class 
 	@Test
 	boolean testIsEmpty() {
 		if(total==0)
@@ -70,6 +81,7 @@ class ObjectListTest {
 		}
 	}
 
+	// a test method that is named testIsFull which is a reflection of the isFull method in the original class
 	@Test
 	boolean testIsFull() {
 		ObjectListTest ol=new ObjectListTest();
@@ -80,6 +92,7 @@ class ObjectListTest {
 		else return false;
 	}
 		
+	// A test case that tests the below test method testGetItem1
 	@Test
 	void testGetItem1()
 	{
@@ -87,6 +100,7 @@ class ObjectListTest {
 		equals(ol.testGetItem(ol.total));
 	}
 	
+	// A test method named testGetItem that is tested by the above test case 
 	@Test
 	Object testGetItem(int positionIn) {
 		ObjectListTest ol=new ObjectListTest();
@@ -101,12 +115,14 @@ class ObjectListTest {
 		}
 	}
 
+	// A test method that represents the method getTotal in the ObjectList clacc
 	@Test
 	int testGetTotal() {
 		ObjectListTest ol=new ObjectListTest();
 		return ol.total;
 	}
 
+	// A test case that tests the above method testGetTotal
 	@Test
 	void testGetTotal1()
 	{
@@ -114,6 +130,7 @@ class ObjectListTest {
 		assertEquals(0,ol.testGetTotal());
 	}
 	
+	// A test case that tests the below method testRemove
 	@Test
 	void testRemove1()
 	{
@@ -121,10 +138,9 @@ class ObjectListTest {
 			assertFalse(ol.testRemove(total));
 	}
 	
+	// A test method that represents the method Remove in the ObjectList class 
 	@Test
 	boolean testRemove(int numberIn) {
-		
-	
 		
 		if(numberIn >= 1 && numberIn <= total)
 		{
@@ -140,6 +156,4 @@ class ObjectListTest {
 			return false;
 		}
 	}
-
-
 }
